1f3a982955d59abbd503bddaf64a01cff85bff54
Shaurav <bhattshaurav404@gmail.com>
2025-01-27 18:38:30 +0530

    Variable & Constant covered

---
+++ Javascript Series ( Code + Project) /Basics/Lecture-54/script.js
@@ -0,0 +1,25 @@
+//Variables & Constants
+    // variables-> whose value can be changed !! (var, let)
+    // constants-> whose values are fixed and can't be changed !! (const)
+    // {}-> Block Scope
+var name = "Shaurav";
+let age = 19;
+const userEmail = "bhatt@gmail.com";
+
+console.table([name,age,userEmail]);
+
+name = "shaurav bhatt"
+age = 20;
+// userEmail = "bhatt15@gmail.com"; 
+
+firstVariable = 234;
+firstVariable = 456
+console.log(firstVariable);
+
+//Declaration -> Variable create karna (ex:- let xyz)
+//Initialisation -> Variable create karte hai and sath me value de dete hai (ex:- let xyz = 243;)
+//Assignment -> Variable ko value dena 
+
+var xyz;
+var xyz;
+xyz = 123; //Assigning value
