fcd3952709d63340a977ab42ae2c916e82cccb64
Shaurav <bhattshaurav404@gmail.com>
2025-01-28 00:47:28 +0530

    Understand classification of Data Types

---
+++ Javascript Series ( Code + Project) /Basics/Lecture-58/script.js
@@ -0,0 +1,31 @@
+//Data Types -> which tells us that which type of data is stored by our variable or our constant.
+
+/* 
+
+1.) Primitive Data Type
+    a.) Number , b.) String , c.) BigInt , d.) undefined , e.) Symbols , f.) Boolean , g.) null
+
+2.) Non-Primitive Data Type
+    a.) Array , b.) Object , c.) Function
+*/
+
+
+// let arr = ["Hello" , 13, false]; //Array
+// console.log(arr[1]);
+
+
+// const Myobj = { //Object/
+//     name : "Shaurav",
+//     age : 19
+// }
+// console.log(Myobj.age);
+
+
+// const Myfunction = function() {  //function
+//     console.log("Say Hello");
+    
+// }
+
+// Myfunction()
+
+//Primitive and Non-Primitive DataTypes are created on the basis of how data is stored in the memory and how it is retrived in optimised manner !!
