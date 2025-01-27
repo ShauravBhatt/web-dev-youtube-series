ed6e333b53fba1ea74f6956fdb8a67df0892b181
Shaurav <bhattshaurav404@gmail.com>
2025-01-27 23:50:05 +0530

    Understanding Mathematical Operations

---
+++ Javascript Series ( Code + Project) /Basics/Lecture-56/script.js
@@ -0,0 +1,47 @@
+//Mathematical Operations
+
+let a = 2 ; 
+let b = 4 ;
+
+// console.log(a+b); //2+4 = 6
+// console.log(a-b); //2-4 = -2
+// console.log(a*b); //2*4 = 8
+// console.log(a/b); //2/4 = 0.5
+// console.log(a%b); //2%4 = 2
+// console.log(a**b); //2^4 = 16
+
+let str = "Shaurav "+"Dev";
+// console.log(str); //Shaurav Dev
+
+// console.log(1+2); // Answer is 3
+
+// console.log("1"+2); //Answer 12
+// console.log(1+"2"); //Answer 12
+
+// console.log("1"+2+3+4); //1234
+// console.log(1+2+3+"4"); //64
+
+// console.log(+true); //1
+// console.log(+"hiii"); //Nan
+// console.log(+""); //0
+
+
+/* Post Decrement & Pre Decrement */
+    // Mathematical operation consists of Two Things 1.) Operands 2.) Operator
+    // Post Means (After) , Pre Means (Before)
+    // Post Says First Use Me Then I Update , Pre Says First Update Me Then Use
+
+let num = 7 ;
+console.log(num) ; //7
+
+console.log(num++); //print 7 & update it to 8
+console.log(num--); //print 8 & update it to 7
+
+console.log(++num); // Update it to 8 & then print 8 
+console.log(--num); // Update it to 7 first then print 7
+
+
+
+
+
+
