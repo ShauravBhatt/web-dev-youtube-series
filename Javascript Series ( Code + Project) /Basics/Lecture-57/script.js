814470b71c88ef250fefebd4b7f43a182b9b7474
Shaurav <bhattshaurav404@gmail.com>
2025-01-28 00:18:07 +0530

    Confusing Comparison Of Data Types

---
+++ Javascript Series ( Code + Project) /Basics/Lecture-57/script.js
@@ -0,0 +1,30 @@
+//Datatype Comparison
+
+let a = 1 ;
+let b = 3 ;
+
+// console.log(a<b); //true
+// console.log(a>b); //false
+// console.log(a>=b); //false
+// console.log(a<=b); //true
+// console.log(a!=b); //true
+// console.log(a==b); //false
+// console.log(a===b); //false
+
+// == -> Loose Equality Check (It compares value only) , === -> Strict Equality Check (It compares Values & DataType Both)
+
+// console.log("2">1); //true
+// console.log(2<"01"); //false
+
+
+// console.log(null<0); // false because 0 is not smaller than 0.
+// console.log(null==0); // false because our (==) doesn't convert the null into 0.
+// console.log(null>=0); // true because (<,>) it converts the null into 0.
+
+console.log(null === undefined); //null ka datatype hai object and undefined ka datatype hai undefined !!
+console.log("2"===2); //false
+
+
+
+
+
