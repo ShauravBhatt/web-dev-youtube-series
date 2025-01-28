78591c4bb6c11ba0d7313dc6cd93b09c917099ea
Shaurav <bhattshaurav404@gmail.com>
2025-01-28 18:38:23 +0530

    Learned About Basics of Strings

---
+++ Javascript Series ( Code + Project) /Basics/Lecture-59/script.js
@@ -0,0 +1,32 @@
+//Strings : It's Sequence of Characters !!
+
+// ************************** Part-1 ***************************
+ 
+let str = "Shaurav Bhatt";
+let num = "1 2" ;
+let isLoggedIn = "false";
+// console.log(typeof isLoggedIn);
+
+let str1 = "Hello " 
+let str2 = "world"
+let compStr = str1+str2; //String Concatenations
+// console.log(compStr);
+
+let name = "Shaurav"
+let age = 19 ;
+//console.log("Hello Mr. " + name + "!!" + " Your age is: " + age); //Old Syntax to concatenate strings
+
+//String Interpolation:
+//console.log(`Hello Mr. ${name} !! Your age is: ${age}`) //This is the modern and recommended method 
+
+let myName = "Shaurav"; //Simply creating a string
+
+let myName1 = new String("SHAURAV"); //String Constructer Method
+console.log(myName1.toLowerCase());
+console.log(myName1[3].toLocaleLowerCase())
+console.log(myName1.charAt(4));
+console.log(myName1.indexOf("H"));
+
+
+
+
