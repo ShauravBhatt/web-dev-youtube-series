35e48e1c90efc31ac0593ca7d05228919015c272
Shaurav <bhattshaurav404@gmail.com>
2025-03-06 16:18:50 +0530

    Intro to Nodejs, LibUv, npm, etc. in Detail

---
+++ Nodejs/Lecture-121/script.js
@@ -0,0 +1,19 @@
+//asynchronous example
+
+// const fs = require("fs");
+
+// fs.readFile("file.txt", "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log("File Content: ", data);
+// });
+
+// console.log("Reading file asynchronously...");
+
+
+//synchronous example
+// const fs = require("fs");
+
+// const data = fs.readFileSync("file.txt");
+// console.log(data.toString());
+
+// console.log("The data is printed...")
