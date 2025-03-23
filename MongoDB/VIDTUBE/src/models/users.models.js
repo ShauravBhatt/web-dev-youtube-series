9a9edce543059f3cbaaba247ce13b0a9df126d80
Shaurav <bhattshaurav404@gmail.com>
2025-03-23 15:21:42 +0530

    Lecture-134: Explored hooks, methods, hashing and jwt in mongoose practically

--- MongoDB/VIDTUBE/src/models/users.models.js
+++ MongoDB/VIDTUBE/src/models/users.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema({
   username: {
@@ -43,5 +45,37 @@ const userSchema = new Schema({
   },
 } , {timestamps : true}
 );
+
+userSchema.pre("save", async function(next){
+  if(!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password,10);
+  next();
+});
+
+userSchema.methods.comparePassword = async function(enteredPassword){
+  return await bcrypt.compare(enteredPassword,this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id ,
+      email: this.email,
+      fullname : this.name
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+  )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+  return jwt.sign(
+    {
+      _id: this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 export const User = model("User", userSchema);
