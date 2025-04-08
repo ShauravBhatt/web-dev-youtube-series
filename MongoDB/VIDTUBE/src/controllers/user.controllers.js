a30f88b0b45e751ffa87b50540c200822e36228a
Shaurav <bhattshaurav404@gmail.com>
2025-04-08 21:25:45 +0530

    Lecture-141: Implemeneted aggregation pipeline in our project to fetch user data

--- MongoDB/VIDTUBE/src/controllers/user.controllers.js
+++ MongoDB/VIDTUBE/src/controllers/user.controllers.js
@@ -363,4 +363,121 @@ const updateCoverImage = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200 , user , "User Cover Image Updated !!"));
 });
 
-export { registerUser, loginUser, refreshAccessToken, userLogout , changePassword , getCurrentUser , updateAccountDetails , updateAvatar , updateCoverImage };
+const getUserChannelProfile = asyncHandler(async (req,res)=>{
+  const {username} = req.params ;
+
+  if(!username.trim()){
+    throw new ApiError(400 , "Username doesn't Exist !!")
+  }
+
+  const channel = await User.aggregate(
+    [
+      {
+        $match: {
+          username: username?.trim().toLowerCase()
+        }
+      },
+      {
+        $lookup: {
+          from: "Subscribe",
+          localField: "_id",
+          foreignField: "channel",
+          as: "subscribers"
+        },
+        $lookup: {
+          from: "Subscribe",
+          localField: "_id",
+          foreignField: "subscriber",
+          as: "subscribedTo"
+        },
+        $addField: {
+          subscriberCount: {
+            $size: "$subscribers"
+          },
+          channelSubscribedToCount: {
+            $size: "$subscribedTo"
+          }
+        },
+        isSubscribed: {
+          $cond: {$in: [req.user?._id, "$subscribers.subscriber"]},
+          then: true,
+          else: false
+        }
+      },
+      {
+        $project: {
+          fullname: 1,
+          username: 1,
+          avatar: 1,
+          coverImage: 1,
+          subscriberCount: 1,
+          channelSubscribedToCount: 1,
+          isSubscribed: 1
+        }
+      }
+    ]
+  )
+
+  if(!channel?.length){
+    throw new ApiError(404, "Can't Fetch Channel Data !!");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0] , "Data Fetched Successfully !!"));
+});
+
+const getWatchHistory = asyncHandler(async (req,res)=>{
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user?._id),
+      }
+    },
+    {
+      $lookup: {
+        from: "Video",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          }
+        ]
+
+      }
+    },
+    {
+      $addField: {
+        owner: {
+          $first: "$owner"
+        }
+      }
+    }
+  ]);
+
+  if(!user){
+    throw new ApiError(500 , "Users watch history can't fetched !!");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user[0]?.watchHistory , "Watch History Of User Fetched Successfully !!"))
+});
+
+export { registerUser, loginUser, refreshAccessToken, userLogout , changePassword , getCurrentUser , updateAccountDetails , updateAvatar , updateCoverImage, getUserChannelProfile, getWatchHistory };
