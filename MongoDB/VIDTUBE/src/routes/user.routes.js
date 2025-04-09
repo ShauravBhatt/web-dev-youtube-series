43f359e87e4aae83db9ef9f3a1427c2726b9522c
Shaurav <bhattshaurav404@gmail.com>
2025-04-09 06:29:07 +0530

    Lecture-142: Created routes for user controller

--- MongoDB/VIDTUBE/src/routes/user.routes.js
+++ MongoDB/VIDTUBE/src/routes/user.routes.js
@@ -1,27 +1,57 @@
 import { Router } from "express";
-import { registerUser, userLogout } from "../controllers/user.controllers.js";
-import {upload} from "../middlewares/multer.middlewares.js";
+import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import {
+  changePassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
+  refreshAccessToken,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
+} from "../controllers/user.controllers.js";
 
 const router = Router();
 
+//Unsecure Routes
 router.route("/register").post(
-    upload.fields(
-        [
-            {
-                name:"avatar",
-                maxCount: 1
-            },
-            {
-                name: "coverImage",
-                maxCount:1
-            }
-        ]
-    )
-    ,
-    registerUser);
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+
+router.route("/refresh-token").post(refreshAccessToken);
+
 
 //Secure Routes
-router.route("/logout").post( verifyJWT , userLogout)
+router.route("/logout").post(verifyJWT, userLogout);
+
+router.route("/change-password").post(verifyJWT, changePassword);
+
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+
+router
+  .route("/update-cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+  
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
